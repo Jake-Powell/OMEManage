fd9b99d00c2cd3f1595ae214e3fc64cedff19b89
Jake Powell <jjp68@cam.ac.uk>
2025-07-08 15:29:16 +0100

    Make sure result data.frame names don't overlap between to_match and data

--- R/matching_functions.R
+++ R/matching_functions.R
@@ -297,6 +297,7 @@ match_people_to_data <- function(to_match, data,
                                                   ...)
 
     ret = out$people
+    names(ret) = paste0('NameDB_',names(ret))
     method = out$message
     if(is.null(ret) & !include_non_matched) return(ret)
     if(is.null(ret) & include_non_matched) return(ret)
